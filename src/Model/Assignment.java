20420f86f59e68497c4d46e6510c26adbc507424
Angel Clarke <rlv890@usask.ca>
2025-12-28 18:35:34 -0600

    Finished implementing Assignment.java

--- src/Model/Assignment.java
+++ src/Model/Assignment.java
@@ -1,48 +1,94 @@
 package Model;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Assignment {
-    private String assignmentName;
+    private String name;
     private LocalDate dueDate;
-    private String assignmentStatus;
-    private Integer assignmentPriority;
+    private String status;
+    private Integer priority;
     private String courseCode;
 
+    /**
+     * Initializes object with specified attributes
+     * @param name
+     * @param dueDate
+     * @param courseCode
+     * @param status
+     * @param priority
+     */
     public Assignment(String name, LocalDate dueDate, String courseCode, String status, Integer priority ) {
-
+        this.name = name;
+        this.dueDate = dueDate;
+        this.courseCode = courseCode;
+        this.status = status;
+        this.priority = priority;
     }
 
+    /**
+     * Getter for assignment name
+     * @return String of assignment name
+     */
     public String getName() {
-
+        return name;
     }
+
+    /**
+     * Getter for due date
+     * @return LocalDate of assignment due date
+     */
     public LocalDate getDueDate() {
-
+        return dueDate;
     }
 
+    /**
+     * Getter for status
+     * @return String of assignment status, either "Not started", "In Progress", or "Finished"
+     */
     public String getStatus() {
-
+        return status;
     }
 
+    /**
+     * Getter for priority
+     * @return Int of assignment priority, either 1, 2, or 3, where 3 is the most urgent
+     */
     public int getPriority() {
-
+        return priority;
     }
 
+    /**
+     * Getter for course code
+     * @return String of assignment's course code
+     */
     public String getCourseCode() {
-
+        return courseCode;
     }
 
+    /**
+     * Updates status of assignment
+     * @param status the new status of the assignment
+     */
     public void updateStatus(String status) {
-
+        this.status = status;
     }
+
+    /**
+     * Updates priority of assignment
+     * @param prio the new priority of the assignment
+     */
     public void updatePriority(int prio) {
-
+        this.priority = prio;
     }
 
+    /**
+     * Updates due date of assignment
+     * @param date the new due date of the assignment
+     */
     public void updateDueDate(LocalDate date) {
-
+        this.dueDate = date;
     }
-
 
     public static void main(String[] args) {
         // Test if initialization worked
@@ -53,7 +99,7 @@ public class Assignment {
         }
 
         LocalDate date = LocalDate.of(2025, 10, 28);
-        if (!(assn.getDueDate() = date)) {
+        if (!(Objects.equals(assn.getDueDate(), date))) {
             System.out.println("Error: getDueDate() not returning correct due date after initialization");
         }
 
@@ -63,20 +109,20 @@ public class Assignment {
 
         // Test updateStatus()
         assn.updateStatus("In Progress");
-        if (!(assn.getStatus().equals("InProgress"))) {
+        if (!(assn.getStatus().equals("In Progress"))) {
             System.out.println("Error: assignment did not correctly update status");
         }
 
         // Test updatePriority()
         assn.updatePriority(1);
-        if(!(assn.getPriority() = 1)) {
+        if(!(assn.getPriority() == 1)) {
             System.out.println("Error: Assignment did not update priority correctly");
         }
 
         // Test updateDueDate()
         assn.updateDueDate(LocalDate.of(2025, 10, 25));
-
-        if (!(assn.getDueDate() = LocalDate.of(2025, 10, 25))) {
+        LocalDate newDate = LocalDate.of(2025, 10, 25);
+        if (!(Objects.equals(assn.getDueDate(), newDate))) {
             System.out.println("Error: getDueDate() not returning correct due date after using updateDueDate()");
         }
 
